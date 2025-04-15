14149b4ce0dcc24b7e2713ab60a63c28f709bdc7
majayallen <majayallen@gmail.com>
2025-04-15 02:13:57 +0200

    Add id in DTO Product

--- Ecom.Core/DTO/ProductDTO.cs
+++ Ecom.Core/DTO/ProductDTO.cs
@@ -11,6 +11,7 @@ namespace Ecom.Core.DTO
 {
     public record ProductDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal NewPrice { get; set; }
