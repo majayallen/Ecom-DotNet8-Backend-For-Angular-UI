0380767e620f16b6e38d8261183c62ac333d73b6
majayallen <majayallen@gmail.com>
2025-05-07 02:27:47 +0300

    Add Send Email

---
+++ Ecom.Core/DTO/EmailDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Core.DTO
+{
+    public class EmailDTO
+    {
+        public EmailDTO(string to, string form, string subject, string content)
+        {
+            To = to;
+            Form = form;
+            Subject = subject;
+            Content = content;
+        }
+
+        public string To { get; set; }
+        public string Form { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Ecom.Infrastructure/Repositries/Service/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Core.DTO;
+using Ecom.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Repositries.Service
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmail(EmailDTO emailDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
